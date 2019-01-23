f282196d9237c5761998b1b3e333773b8afe9349
Fredrik Wollsén <fredrik@neam.se>
2019-01-23 11:25:39 +0200

    Placeholder for concept value lookup logic in GM_DATA

--- src/GM_DATA.ts
+++ src/GM_DATA.ts
@@ -1,20 +1,37 @@
 import { getDataGeographiesListOfCountriesEtcLookupTable } from "./dataGeographies";
 
 /**
- * Inserts a property column, including a header row, with a common Gapminder property matched against the input column range.
+ * Inserts a property or concept column, including a header row, with a common Gapminder property or concept matched against the input column/table range.
  *
- * @param column_range_with_headers
- * @param prop
+ * @param column_or_table_range_with_headers Either a column range (for a property lookup column) or a table range including [geo,name,time] (for a concept value lookup)
+ * @param prop_or_concept_id
  * @return A two-dimensional array containing the cell/column contents described above in the summary.
  */
-export function GM_DATA(column_range_with_headers: string[][], prop: string) {
-  const lookupTable = getDataGeographiesListOfCountriesEtcLookupTable();
-
+export function GM_DATA(
+  column_or_table_range_with_headers: string[][],
+  prop_or_concept_id: string
+) {
   // Clone the input param to prevent side effects
-  const inputColumn = column_range_with_headers.concat([]);
+  const inputColumnOrTable = column_or_table_range_with_headers.concat([]);
 
-  // Drop the input range header row
-  inputColumn.shift();
+  // Separate the input range header row
+  const inputColumnOrTableHeaderRow = inputColumnOrTable.shift();
+
+  if (inputColumnOrTableHeaderRow.length === 1) {
+    return dataGeographiesListOfCountriesEtcPropertyLookup(
+      inputColumnOrTable,
+      prop_or_concept_id
+    );
+  } else {
+    return conceptValueLookup(inputColumnOrTable, prop_or_concept_id);
+  }
+}
+
+/**
+ * @hidden
+ */
+function dataGeographiesListOfCountriesEtcPropertyLookup(inputColumn, prop) {
+  const lookupTable = getDataGeographiesListOfCountriesEtcLookupTable();
 
   // Convert the prop to the Gsheet-generated equivalent property (eg "UN member since" becomes "unmembersince")
   const gsxProperty = prop.toLowerCase().replace(/[^A-Za-z0-9]*/g, "");
@@ -32,4 +49,11 @@ export function GM_DATA(column_range_with_headers: string[][], prop: string) {
   });
 
   return [[prop]].concat(matchedData);
+}
+
+/**
+ * @hidden
+ */
+function conceptValueLookup(inputTable, conceptId) {
+  return [["foo"]];
 }
