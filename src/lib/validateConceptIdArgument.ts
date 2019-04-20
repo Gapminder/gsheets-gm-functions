f1adf110260a1ed609793b4be45165f1d33b607e
Fredrik Wollsén <fredrik@neam.se>
2019-04-20 14:23:06 +0300

    Including the invalid concept id in error message for easier troubleshooting

--- src/lib/validateConceptIdArgument.ts
+++ src/lib/validateConceptIdArgument.ts
@@ -3,11 +3,13 @@
  */
 export function validateConceptIdArgument(concept_id) {
   if (concept_id.match(/^ /) || concept_id.match(/ $/)) {
-    throw new Error("The concept id should not start or end with a space");
+    throw new Error(
+      `The concept id ("${concept_id}") should not start or end with a space`
+    );
   }
   if (!concept_id.match(/^([a-z0-9_]*)$/)) {
     throw new Error(
-      "The concept id may only contain alphanumeric characters (a-z, 0-9) and underscores"
+      `The concept id ("${concept_id}") may only contain alphanumeric characters (a-z, 0-9) and underscores`
     );
   }
 }
